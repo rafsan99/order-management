154cac866275cbddd550b8e4ca9af4af13267f43
rafsan99 <rafsan.redwan69@gmail.com>
2024-11-30 21:31:47 +0600

    chore: model declared for User

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint      `gorm:"primaryKey"`
+	Email     string    `gorm:"unique;not null"`
+	Password  string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
